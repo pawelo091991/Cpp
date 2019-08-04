f5d8b04e7d7d61a07486dc8384bb048385ffea9d
Pawel Sporna <pawel.sporna@gmail.com>
2019-08-04 11:04:48 +0200

    Added minor changes

---
+++ sum-of-multiples/sum_of_multiples.cpp
@@ -0,0 +1,5 @@
+#include "sum_of_multiples.h"
+
+uint32_t sum_of_multiples::to(std::pair<uint32_t, uint32_t> input, uint32_t) {
+	return 0;
+}
---
+++ sum-of-multiples/sum_of_multiples.h
@@ -0,0 +1,5 @@
+#include <cstdint>
+#include <utility>
+namespace sum_of_multiples {
+	uint32_t to(std::pair<uint32_t, uint32_t> input, uint32_t);
+}
